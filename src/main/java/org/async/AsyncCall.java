a7be4683dfd55859179e49cff2f28ae9efe01b8a
hongyuanChrisLi <hychrisli@users.noreply.github.com>
2018-06-07 13:46:28 -0700

    POST

--- src/main/java/org/async/AsyncCall.java
+++ src/main/java/org/async/AsyncCall.java
@@ -2,18 +2,21 @@ package org.async;
 
 import org.asynchttpclient.*;
 
+import java.io.IOException;
+
 public class AsyncCall {
 
     private AsyncHttpClient client;
-    private RequestBuilder builder;
+    private RequestBuilder getBuilder, postBuilder;
 
     public AsyncCall () {
-        builder = new RequestBuilder("GET");
+        getBuilder = new RequestBuilder("GET");
+        postBuilder = new RequestBuilder("POST");
         client = Dsl.asyncHttpClient();
     }
 
     public void get(String url){
-        Request request = builder.setUrl(url).build();
+        Request request = getBuilder.setUrl(url).build();
         client.executeRequest(request, new AsynGetHandler());
         System.out.println("Hello there, aync baby!");
 
@@ -22,12 +25,25 @@ public class AsyncCall {
     }
 
     public void post(String url, String body){
-
+        Request request = postBuilder.setUrl(url).setBody(body).build();
+        client.executeRequest(request, new AsynGetHandler());
+        System.out.println("Hello there, aync POST baby!");
     }
 
-    public static void main (String[] args) {
+    public void close() throws IOException {
+    }
+
+    public static void main (String[] args) throws IOException {
         AsyncCall asyncCall = new AsyncCall();
-        asyncCall.get("https://www.google.com");
+        // asyncCall.get("https://www.google.com");
+        asyncCall.post("https://jsonplaceholder.typicode.com/posts",
+                "{\n" +
+                "  \"title\": \"foo\",\n" +
+                "  \"body\": \"bar\",\n" +
+                "  \"userId\": 1\n" +
+                "}");
+        asyncCall.get("https://jsonplaceholder.typicode.com/posts/101");
+        asyncCall.get("https://jsonplaceholder.typicode.com/posts/100");
 
     }
 }
